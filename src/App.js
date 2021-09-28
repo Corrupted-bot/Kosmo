f35e67e719f88cb660eb5e63ce19f88883f049ad
Gustavo Cheng <Gusgus@MacBook-Pro-de-Gus.local>
2021-09-28 19:45:47 -0300

    login/backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [usernameReg, setUsernameReg] = useState ("");
+  const [passwordReg, setPasswordReg] = useState ("");
+
+  const [username, setUsername] = useState ("");
+  const [password, setPassword] = useState ("");
+
+  const register = () => {
+    Axios.post('http://localhost3001/register', {
+      username: usernameReg, 
+      password: passwordReg,
+  }).then((response) => {
+    console.log(response)
+  });
+}
+
+const login = () => {
+  Axios.post('http://localhost3001/login', {
+    username: username, 
+    password: password,
+  }).then((response) => {
+    console.log(response)
+  });
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <div className="registro">
+        <h1> Registro </h1>
+        <label> Usuario </label>
+        <input type="text" 
+        onChange={(e) => { 
+          setUsernameReg(e.target.value);
+         }}
+          />
+        <label> Contraseña </label>
+        <input type="password" 
+        onChange={(e) => { 
+          setPasswordReg(e.target.value);
+         }}/>
+        <button onClick={register}> Registrarse </button>
+      </div>
+      <div className = "login">
+        <h1> Login </h1>
+        <label> Usuario </label>
+        <input type="text" 
+        onChange={(e) => { 
+          setUsername(e.target.value);
+         }}/>
+        <label>Contraseña</label>
+        <input type="password" 
+        onChange={(e) => { 
+          setPassword(e.target.value);
+         }}/>
+        <button onClick={login}> Entrar </button>
+      </div>
     </div>
   );
 }
