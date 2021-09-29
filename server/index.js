887949dd9ff3506c002fa50288e7c78aa66b5023
Diego Lopez <diego.lopez2000@hotmail.com>
2021-09-29 19:07:27 -0300

    rutas_creadas - backend - navbar-loginyregistro

--- server/index.js
+++ server/index.js
@@ -1,46 +1,76 @@
 const express = require("express");
 const mysql = require("mysql");
 
-const app = express();
-
-app.use(express.json());
 
 const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "password",
-  database: "LoginSystem",
+  user: "grupo07",
+  host: "icf233.c5d4mi2dthpc.us-east-1.rds.amazonaws.com",
+  password: "grupo..07",
+  database: "grupo07",
 });
 
+//Estatus de conexión
+db.connect(function(err){
+    if (err){
+        console.error('error connecting: ' + err.stack);
+    }
+    else{
+        console.log('MySQL Conectado!');
+    }
+});
+
+// SETUP DEL SERVIDOR
+const app = express();
+app.use(express.json());
+
+const cors=require("cors");
+app.use(cors());
+
+
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
+
+// app.use(cors(corsOptions))
+//Puerto de el servidor
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.send('TESTING');
+});
+
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query("SELECT * FROM users WHERE username = ? AND password = ?",[username, password], 
+        (err, result) => {
+        if(err){
+            res.send({ err: err });
+        }
+        if(result.length > 0){
+            res.send(result);
+        } else {
+            res.send({message: "Contraseña o Usuario incorrecto."});
+          }
+      });
+});
+//REGISTRO CORRECTO
 app.post('/register', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    db.query("INSERT INTO users (username, password) VALUES (?, ?)",
-        [username, password], 
+    db.query("INSERT INTO users (username, password) VALUES (?, ?)",[username, password], 
         (err, result) => {
         console.log(err);
     });
 });
 
-app.post('login/', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-  
-    db.query("SELECT*FROM users WHERE username =? AND password = ?)",
-        [username, password], 
-        (err, result) => {
-        if(err){
-            res.send({err: err})
-        }
-        if(result){
-            res.send(result)
-        } else {
-            res.send({message: "No user found"})
-          }
-      });
-});
 
-app.listen(3001, () => {
-  console.log("running server");
+//Iniciando la api 
+app.listen(PORT, () => {
+    console.log(`El servidor esta corriendo en el puerto ${PORT}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,66 +1,53 @@
-import React, { useState } from "react";
-import Axios from "axios";
+import React from "react";
 import './App.css';
+
+//Componentes
+import Login from "./components/Login";
+import Registro from "./components/Registro";
+import Inicio from "./components/Inicio";
+
+
+// Pack install 
+// npm install react-router-dom
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
 
 function App() {
 
-  const [usernameReg, setUsernameReg] = useState ("");
-  const [passwordReg, setPasswordReg] = useState ("");
-
-  const [username, setUsername] = useState ("");
-  const [password, setPassword] = useState ("");
-
-  const register = () => {
-    Axios.post('http://localhost3001/register', {
-      username: usernameReg, 
-      password: passwordReg,
-  }).then((response) => {
-    console.log(response)
-  });
-}
-
-const login = () => {
-  Axios.post('http://localhost3001/login', {
-    username: username, 
-    password: password,
-  }).then((response) => {
-    console.log(response)
-  });
-}
-
-
   return (
-    <div className = "App">
-      <div className="registro">
-        <h1> Registro </h1>
-        <label> Usuario </label>
-        <input type="text" 
-        onChange={(e) => { 
-          setUsernameReg(e.target.value);
-         }}
-          />
-        <label> Contraseña </label>
-        <input type="password" 
-        onChange={(e) => { 
-          setPasswordReg(e.target.value);
-         }}/>
-        <button onClick={register}> Registrarse </button>
-      </div>
-      <div className = "login">
-        <h1> Login </h1>
-        <label> Usuario </label>
-        <input type="text" 
-        onChange={(e) => { 
-          setUsername(e.target.value);
-         }}/>
-        <label>Contraseña</label>
-        <input type="password" 
-        onChange={(e) => { 
-          setPassword(e.target.value);
-         }}/>
-        <button onClick={login}> Entrar </button>
-      </div>
+    <Router>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+              <NavLink to="/" className="btn btn-dark" activeClassName="disable" >Inicio</NavLink>
+              </li>
+              <li class="nav-item">
+              <NavLink to="/login" className="btn btn-dark"  activeClassName="active">Iniciar Sesion</NavLink>
+              </li>
+              <li class="nav-item">
+              <NavLink to="/registro" className="btn btn-dark" activeClassName="active">Registro</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className = "App App-header">
+        <Switch>
+          <Route path="/registro"><Registro/></Route>
+          <Route path="/login"><Login/></Route>
+          <Route path="/"><Inicio/></Route>
+        </Switch>
     </div>
+    </Router>
   );
 }
 
